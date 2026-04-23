34d728f3d71cd0af9a2c05092a21415503e5fa08
programmerShinobi <shinobiprogrammer@gmail.com>
2026-04-23 14:06:17 +0700

    [add] setup project

---
+++ src/main/java/programmershinobi/spring/BelajarSpringAsyncWithJavaVersion26Application.java
@@ -0,0 +1,13 @@
+package programmershinobi.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringAsyncWithJavaVersion26Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringAsyncWithJavaVersion26Application.class, args);
+	}
+
+}
---
+++ src/test/java/programmershinobi/spring/BelajarSpringAsyncWithJavaVersion26ApplicationTests.java
@@ -0,0 +1,13 @@
+package programmershinobi.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringAsyncWithJavaVersion26ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
